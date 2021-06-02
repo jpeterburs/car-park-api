072de42d8cc6db1359b1f64cdaf8829f889ebf23
jpeterburs <j.peterburs@outlook.de>
2021-06-02 11:21:20 +0000

    Add session controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,44 @@
+module Api
+  class SessionsController < ApplicationController
+    before_action :load_session, only: %i[show update]
+
+    def show
+    end
+
+    def create
+      @session = Session.new(permitted_params)
+      @session.assign_attributes(entered_at: DateTime.now)
+
+      if @session.save
+        render json: {}, status: :created
+      else
+        render json: { errors: @session.errors }, status: :bad_request
+      end
+    end
+
+    def update
+      @session.assign_attributes(exited_at: DateTime.now)
+
+      if @session.save
+        render json: {}, status: :accepted
+      else
+        render json: { errors: @session.errors }, status: :bad_request
+      end
+    end
+
+    private
+
+    def load_session
+      @session = Session.find(params[:id])
+    end
+
+    def permitted_params
+      params[:session]&.permit(
+        %i[
+          license_plate
+          permanent_parker_id
+        ]
+      )
+    end
+  end
+end
