371d5aa942ef6baa73d25c1159a0fd08a863c818
jpeterburs <j.peterburs@outlook.de>
2021-06-16 08:12:14 +0000

    Remove total active sessions from api view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
     render json: {
       max_capacity: Session::MAX_CAPACITY,
       reserved: Session::RESERVED,
-      active_sessions: active_sessions.count,
       normal_sessions: active_sessions.without_permanent_parker.count,
       permanent_parker_sessions: active_sessions.with_permanent_parker.count
     }
