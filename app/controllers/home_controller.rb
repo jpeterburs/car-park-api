da194b85b46fe93f9dc7c56b92d0f46808d09d73
jpeterburs <j.peterburs@outlook.de>
2021-06-16 06:50:36 +0000

    Add parking_space overview

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,17 @@
 class HomeController < ApplicationController
   def index
     render json: {
-      status: 'ok',
-      message: 'API working hard'
+      max_capacity: Session::MAX_CAPACITY,
+      reserved: Session::RESERVED,
+      active_sessions: active_sessions.count,
+      normal_sessions: active_sessions.without_permanent_parker.count,
+      permanent_parker_sessions: active_sessions.with_permanent_parker.count
     }
+  end
+
+  private
+
+  def active_sessions
+    Session.active
   end
 end
